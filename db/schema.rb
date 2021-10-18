7f58cd87eeb5590649140be0efbc346c53abedb6
Guhan M <60909829+guhan29@users.noreply.github.com>
2021-10-18 07:56:27 +0000

    added review controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_042509) do
+ActiveRecord::Schema.define(version: 2021_10_18_071817) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,15 @@ ActiveRecord::Schema.define(version: 2021_10_18_042509) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.integer "category_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "play_id"
   end
 
   create_table "users", force: :cascade do |t|
