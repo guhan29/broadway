2879dd46ec6f7f623def78430d09a0762722f157
guhan29 <guhan29@gmail.com>
2021-10-18 04:04:46 +0000

    initial commit

---
+++ app/controllers/plays_controller.rb
@@ -0,0 +1,53 @@
+class PlaysController < ApplicationController
+  before_action :find_play, only: [:show, :edit, :update, :destroy]
+  def index
+    @plays = Play.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def edit
+    @categories = Category.all.map{ |c| [c.name, c.id] }
+  end
+
+  def update
+    @play.category_id = params[:category_id]
+    if @play.update(play_params)
+      redirect_to play_path(@play)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @play.destroy
+    redirect_to root_path
+  end
+
+  def new
+    @play = current_user.plays.build
+    @categories = Category.all.map{ |c| [c.name, c.id] }
+  end
+
+  def create
+    @play = current_user.plays.build(play_params)
+    @play.category_id = params[:category_id]
+
+    if @play.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def play_params
+    params.require(:play).permit(:title, :description, :director, :category_id)
+  end
+
+  def find_play
+    @play = Play.find(params[:id])
+  end
+end
